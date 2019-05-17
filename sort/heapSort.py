7ee4ece5670d0d0ac0fbd09753a55775db0d7dfc
金一成 <jinyicheng@jinyichengdembp.lan>
2019-05-17 16:29:09 +1000

    algorithm

---
+++ sort/heapSort.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+# 堆排序
+# 每趟排序选出最大值，放在数组末尾
+# 最后一个非叶子结点 n/2
+
+def heapSort(dataset):
+	if len(dataset)<=1:
+		return dataset
+	n=len(dataset)
+	# 创造初始堆
+	for i in range(n/2-1,-1,-1):
+		shift(dataset,i,n-1)
+	for i in range(n-1,-1,-1):
+		dataset[i],dataset[0]=dataset[0],dataset[i]
+		shift(dataset,0,i-1)
+
+def shift(dataset, lo, hi):
+	i=lo
+	j=2*i+1
+	temp=dataset[i]
+	while j<=hi:
+		if j<hi and dataset[j+1]>dataset[j]:
+			j+=1
+		if temp<dataset[j]:
+			dataset[i]=dataset[j]
+			i=j
+			j=2*i+1
+		else:
+			break
+	dataset[i]=temp
+
+if __name__ == '__main__':
+	dataset=[6,3,2,7,4,2,5,8]
+	heapSort(dataset)
+	print dataset
---
+++ sort/mergeSort.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+# 归并排序
+# 分治法，分而治之
+
+def mergeSort(dataset):
+	if len(dataset)<=1:
+		return dataset
+	mid=len(dataset)/2
+	left=mergeSort(dataset[:mid])
+	right=mergeSort(dataset[mid:])
+	return merge(left, right)
+
+def merge(left, right):
+	res=[]
+	i=0
+	j=0
+	while i<len(left) and j<len(right):
+		if left[i]<right[j]:
+			res.append(left[i])
+			i+=1
+		else:
+			res.append(right[j])
+			j+=1
+	if i==len(left):
+		res.extend(right[j:])
+	else:
+		res.extend(left[i:])
+	return res
+
+if __name__ == '__main__':
+	dataset=[6,3,2,7,4,2,5,8]
+	print mergeSort(dataset)
